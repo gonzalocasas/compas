a39777855e06a159780d15e75e02cf09191b607a
Tom Van Mele <vanmelet@ethz.ch>
2018-01-02 12:57:37 +0100

    initial commit of interface functionality

--- src/compas/datastructures/assembly/assembly.py
+++ src/compas/datastructures/assembly/assembly.py
@@ -19,6 +19,34 @@ class Assembly(Network):
 
     def __init__(self):
         super(Assembly, self).__init__()
+        self.blocks = {}
+        self.default_vertex_attributes.update({
+            'is_support': False
+        })
+        self.default_edge_attributes.update({
+            'interface_points' : None,
+            'interface_type'   : None,
+            'interface_size'   : None,
+            'interface_uvw'    : None,
+            'interface_origin' : None,
+            'interface_forces' : None,
+        })
+
+    def add_block(self, block, attr_dict=None, **kwattr):
+        x, y, z = block.centroid()
+        key = self.add_vertex(attr_dict=attr_dict, x=x, y=y, z=z, **kwattr)
+        self.blocks[key] = block
+
+    def add_support(self, block, attr_dict=None, **kwattr):
+        x, y, z = block.centroid()
+        key = self.add_vertex(attr_dict=attr_dict, x=x, y=y, z=z, is_support=True, **kwattr)
+        self.blocks[key] = block
+
+    def add_interface(self):
+        pass
+
+    def identify_interfaces(self):
+        pass
 
 
 # ==============================================================================
